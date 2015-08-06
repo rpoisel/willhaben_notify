eb5e8e175112ebebed88921f57b4db8fd4e9f7c1
Rainer Poisel <rainer.poisel@gmail.com>
2015-08-06 22:33:07 +0200

    list and help commands implemented

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,3 +1,4 @@
+from wn_db import User, Subscription, Url
 
 
 class Interpreter(object):
@@ -13,17 +14,38 @@ class Interpreter(object):
         if tokens[0] in commands:
             commands[tokens[0]].execute(self.__scheduler, self.__dbsession, peer, tokens[1:])
             return
-        peer.send_msg('Command not understood.')
+        peer.send_msg('Command not found.')
 
 
 class Command(object):
     def execute(self, scheduler, dbsession, peer, arguments):
         peer.send_msg('Command not implemented')
 
+    def help(self):
+        return 'No help available'
+
 
 class CommandList(Command):
     def execute(self, scheduler, dbsession, peer, arguments):
-        peer.send_msg('Listing subscriptions ...')
+        subscriptions = ''
+        for user, subscription, url in dbsession.query(User, Subscription, Url).filter(User.first_name == peer.first_name).filter(User.last_name == peer.last_name).filter(User.id == Subscription.user_id).filter(Subscription.url_id == Url.id):
+            subscriptions += url.location + ' every ' + str(subscription.query_period) + ' seconds\n'
+        peer.send_msg(subscriptions)
+
+    def help(self):
+        return 'list ... list subscriptions this peer is subscribed to'
+
+
+class CommandHelp(Command):
+    def execute(self, scheduler, dbsession, peer, arguments):
+        helpInfo = ''
+        for command in commands.values():
+            helpInfo += command.help() + '\n'
+        peer.send_msg(helpInfo)
+
+    def help(self):
+        return 'help ... information about available commands'
 
 commands = {}
 commands['list'] = CommandList()
+commands['help'] = CommandHelp()
